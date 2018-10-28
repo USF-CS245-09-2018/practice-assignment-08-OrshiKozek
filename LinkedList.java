573209e17bbc6306c8c898b39cc40361173f14a1
OrshiKozek <okozek@dons.usfca.edu>
2018-10-28 14:26:05 -0700

    LinkedList.java added

---
+++ LinkedList.java
@@ -0,0 +1,27 @@
+public class LinkedList implements  List {
+
+    @Override
+    public void add(int pos, Object obj) {
+
+    }
+
+    @Override
+    public void add(Object obj) {
+
+    }
+
+    @Override
+    public Object remove(int pos) {
+        return null;
+    }
+
+    @Override
+    public Object get(int pos) {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
